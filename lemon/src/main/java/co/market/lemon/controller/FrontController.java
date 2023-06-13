8909e161e09686672e33174159477882ca410406
admin <admin@YD02-08>
2023-06-13 17:59:01 +0900

    product 하는중

--- lemon/src/main/java/co/market/lemon/controller/FrontController.java
+++ lemon/src/main/java/co/market/lemon/controller/FrontController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.market.lemon.command.MainCommand;
+import co.market.lemon.command.ProductInsert;
 import co.market.lemon.common.Command;
 import co.market.lemon.member.command.AjaxCheckId;
 import co.market.lemon.member.command.MemberInsert;
@@ -20,6 +21,8 @@ import co.market.lemon.member.command.MemberInsertForm;
 import co.market.lemon.member.command.MemberLogin;
 import co.market.lemon.member.command.MemberLoginForm;
 import co.market.lemon.mypage.command.Mypage;
+import co.market.lemon.product.command.ProductInsertForm;
+import co.market.lemon.product.command.ProductList;
 
 /**
  * Servlet implementation class FrontController
@@ -47,9 +50,11 @@ public class FrontController extends HttpServlet {
 		map.put("/memberInsert.do", new MemberInsert());
 		map.put("/memberLogin.do", new MemberLogin());
 		map.put("/ajaxCheckId.do", new AjaxCheckId());
-		map.put(" ㅅㄷㄴㅅㅅㄴㅅㄱ", null);
 		
 		map.put("/mypage.do", new Mypage());
+		map.put("/productList.do", new ProductList());
+		map.put("/productInsertForm.do", new ProductInsertForm());
+		map.put("/productInsert.do", new ProductInsert());
 		
 		
 	}
---
+++ lemon/src/main/java/co/market/lemon/product/service/ProductMapper.java
@@ -0,0 +1,18 @@
+package co.market.lemon.product.service;
+
+import java.util.List;
+
+public interface ProductMapper {
+	List<ProductVO> productSelectList();
+
+	ProductVO productSelect(ProductVO vo);
+
+	int productInsert(ProductVO vo);
+
+	int productUpdate(ProductVO vo);
+
+	int productDelete(ProductVO vo);
+
+	ProductVO productLogin(ProductVO vo);
+}
+
