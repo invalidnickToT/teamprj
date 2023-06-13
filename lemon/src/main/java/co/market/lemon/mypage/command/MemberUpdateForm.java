031ec36be7042e380cff767c812dc43e4d8cf698
admin <100tvlha@gmail.com>
2023-06-13 17:20:21 +0900

    0613 회원정보수정 commit

---
+++ lemon/src/main/java/co/market/lemon/mypage/command/MemberUpdateForm.java
@@ -0,0 +1,15 @@
+package co.market.lemon.mypage.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.market.lemon.common.Command;
+
+public class MemberUpdateForm implements Command {
+
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		return "mypage/memberUpdate";
+	}
+
+}
