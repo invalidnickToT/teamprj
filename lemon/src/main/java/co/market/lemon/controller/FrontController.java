d4f8821d090f206075ba9346b58aa6beb9fbed95
admin <100tvlha@gmail.com>
2023-06-15 15:53:06 +0900

    송이 커밋

--- lemon/src/main/java/co/market/lemon/controller/FrontController.java
+++ lemon/src/main/java/co/market/lemon/controller/FrontController.java
@@ -39,8 +39,11 @@ import co.market.lemon.notice.command.noticeSelect;
 import co.market.lemon.notice.command.noticeUpdate;
 import co.market.lemon.notice.command.noticeUpdateForm;
 import co.market.lemon.product.command.ProductDelete;
+import co.market.lemon.product.command.ProductDeleteForm;
 import co.market.lemon.product.command.ProductInsert;
 import co.market.lemon.product.command.ProductInsertForm;
+import co.market.lemon.product.command.ProductListBuy;
+import co.market.lemon.product.command.ProductListSell;
 
 import co.market.lemon.product.command.ProductSelect;
 import co.market.lemon.product.command.ProductUpdate;
@@ -89,12 +92,16 @@ public class FrontController extends HttpServlet {
 
     	//혜련zone
 
+		map.put("/productListBuy.do", new ProductListBuy());
+		map.put("/productListSell.do", new ProductListSell());
+
 		map.put("/productInsertForm.do", new ProductInsertForm());
 		map.put("/productInsert.do", new ProductInsert());
+
 		map.put("/productUpdateForm.do", new ProductUpdateForm());
 		map.put("/productUpdate.do", new ProductUpdate());
+		map.put("/productDeleteForm.do", new ProductDeleteForm());
 		map.put("/productDelete.do", new ProductDelete());
-		
 	
 
 
--- lemon/src/main/java/co/market/lemon/product/command/ProductList.java
+++ lemon/src/main/java/co/market/lemon/product/command/ProductList.java
@@ -15,6 +15,7 @@ public class ProductList implements Command {
 	public String exec(HttpServletRequest request, HttpServletResponse reponse) {
 		ProductService ps = new ProductServiceImpl();
 		List<ProductVO> products = new ArrayList<>();
+		products = ps.productSelectList();
 		
 		request.setAttribute("products", products);
 		return "product/productList";
